0b4d77d04b13780d84055da6b5ffd01843771068
Suyoung <sueyoungkim69@gmail.com>
2026-07-08 01:42:59 +0900

    1-16

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -6,27 +6,27 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
-		Member aaron = Member.builder()
-				.id(1)
+		String request = "AARON";
+		Member.MemberBuilder builder = Member.builder()
 				.age(10)
-				.name("Aaron")
-				.email("aaron@example.com")
-				.build();
-		Member baron = Member.builder()
-				.email("baron@example.com")
-				.name("Baron")
-				.age(20)
-				.id(2)
-				.build();
+				.email("common@example.com");
+
+		if(request.equals("AARON")){
+			builder
+					.id(1)
+					.name("Aaron");
+		} else if (request.equals("BARON")) {
+			builder
+					.id(2)
+					.name("Baron");
+		}
+
+		Member aaron = builder.build();
+
 
 		System.out.println("---");
 		System.out.println(aaron);
 		System.out.println(aaron.toString());
 		System.out.println(aaron.getName());
-
-		System.out.println("---");
-		System.out.println(baron);
-		System.out.println(baron.toString());
-		System.out.println(baron.getName());
 	}
 }

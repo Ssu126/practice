5202d73279c58623644b41cc4929c39e0e39f4cc
Suyoung <sueyoungkim69@gmail.com>
2026-07-07 15:59:32 +0900

    2-13완

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,12 +1,14 @@
 package com.example.demo;
 
+import com.example.demo.member.Administrator;
 import com.example.demo.member.Member;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
 	public static void main(String[] args) {
-		Member aaron = new Member(1, "Aaron", 10, "aaron@example.com");
+		//Member aaron = new Member(1, "Aaron", 10, "aaron@example.com");
+		Administrator aaron = new Administrator(1, "Aaron", 10, "aaron@example.com", "DEVELOPER");
 
 		System.out.println("---");
 		System.out.println(aaron);

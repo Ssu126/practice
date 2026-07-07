bb57c489e9fb8830de60647b6ca757c915128a37
Suyoung <sueyoungkim69@gmail.com>
2026-07-07 15:34:47 +0900

    2-10완

--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -3,15 +3,13 @@ package com.example.demo.member;
 import lombok.*;
 
 @Getter
-@ToString
+@ToString(of = {"id", "name"})
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
 public class Member {
     private Integer id;
     private String name;
     @Setter(AccessLevel.PRIVATE)
-    @ToString.Exclude
     private int age;
     @Setter
-    @ToString.Exclude
     private String email;
 }

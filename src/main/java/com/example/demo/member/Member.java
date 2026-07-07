9a1c2de9b50226cb27969fac19eca5ff73a5f6da
Suyoung <sueyoungkim69@gmail.com>
2026-07-07 18:05:35 +0900

    2-30완

--- src/main/java/com/example/demo/member/Member.java
+++ src/main/java/com/example/demo/member/Member.java
@@ -1,21 +1,25 @@
 package com.example.demo.member;
 
 import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import java.util.logging.Level;
 
 @Getter
 @Setter
 @ToString(onlyExplicitlyIncluded = true, includeFieldNames = false)
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @AllArgsConstructor(access = AccessLevel.PUBLIC)
+@FieldDefaults(level = AccessLevel.PRIVATE)
 public class Member {
     @ToString.Include(name = "memberId")
     @EqualsAndHashCode.Include
-    private Integer id;
+    Integer id;
     @ToString.Include(rank = -1)
     @EqualsAndHashCode.Include
-    private String name;
-    private int age;
-    private String email;
+    String name;
+    String email;
+    int age;
 
     /*
     public boolean equals(Member member){

c77b930d42a4989716a47c1ace1624745e91278e
Suyoung <sueyoungkim69@gmail.com>
2026-07-14 13:02:57 +0900

    3-5

--- src/main/java/com/example/demo1/DemoApplication.java
+++ src/main/java/com/example/demo1/DemoApplication.java
@@ -29,12 +29,30 @@ public class DemoApplication {
         intelligence.attack();
         intelligence.ultimate();
 
-        String[] string_array = new String[]{"1", "2", "3"};
-        System.out.println(string_array.length);
-        Integer[] integer_array = new Integer[]{1, 2, 3};
-        System.out.println(integer_array.length);
+        List<String> string_list = Arrays.asList("1", "2", "3");
+        string_list.get(3);
+        string_list.add("4");
+        string_list.set(3, "5");
+        string_list.remove(3);
+        string_list.subList(0, 1);
+        string_list.sort(String::compareTo);
+        string_list.contains("1");
+        string_list.clear();
+        string_list.isEmpty();
+        string_list.size();
 
-        System.out.println(string_array);
-        System.out.println(integer_array);
+        List<Integer> integer_list = Arrays.asList(1, 2, 3);
+        integer_list.get(3);
+        integer_list.add(4);
+        integer_list.set(3, 5);
+        integer_list.remove(3);
+        integer_list.subList(0, 1);
+        integer_list.sort(Integer::compareTo);
+        integer_list.clear();
+        integer_list.isEmpty();
+        integer_list.size();
+        
+        System.out.println(string_list);
+        System.out.println(integer_list);
     }
 }

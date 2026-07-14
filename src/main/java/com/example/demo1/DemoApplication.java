ee229b9fd7c8ef8e77d9ee2dca552020f915a3bd
Suyoung <sueyoungkim69@gmail.com>
2026-07-14 13:18:51 +0900

    3-6

--- src/main/java/com/example/demo1/DemoApplication.java
+++ src/main/java/com/example/demo1/DemoApplication.java
@@ -6,9 +6,7 @@ import com.example.demo1.StrengthHero;
 import com.example.demo1.Hero;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 @SpringBootApplication
 public class DemoApplication {
@@ -29,30 +27,23 @@ public class DemoApplication {
         intelligence.attack();
         intelligence.ultimate();
 
-        List<String> string_list = Arrays.asList("1", "2", "3");
-        string_list.get(3);
-        string_list.add("4");
-        string_list.set(3, "5");
-        string_list.remove(3);
-        string_list.subList(0, 1);
-        string_list.sort(String::compareTo);
-        string_list.contains("1");
-        string_list.clear();
-        string_list.isEmpty();
-        string_list.size();
+        Set<String> string_set = new HashSet<>(Arrays.asList("1", "2", "3"));
+        string_set.add("4");
+        string_set.remove("1");
+        string_set.contains("1");
+        string_set.clear();
+        string_set.isEmpty();
+        string_set.size();
 
-        List<Integer> integer_list = Arrays.asList(1, 2, 3);
-        integer_list.get(3);
-        integer_list.add(4);
-        integer_list.set(3, 5);
-        integer_list.remove(3);
-        integer_list.subList(0, 1);
-        integer_list.sort(Integer::compareTo);
-        integer_list.clear();
-        integer_list.isEmpty();
-        integer_list.size();
-        
-        System.out.println(string_list);
-        System.out.println(integer_list);
+        Set<Integer> integer_set = new HashSet<>(Arrays.asList(1, 2, 3));
+        integer_set.add(4);
+        integer_set.remove(3);
+        integer_set.contains(1);
+        //integer_set.clear();
+        integer_set.isEmpty();
+        integer_set.size();
+
+        System.out.println(string_set);
+        System.out.println(integer_set);
     }
 }

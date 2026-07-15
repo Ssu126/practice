47985996216d1c2f2d67a537744421de1a8253b5
Suyoung <sueyoungkim69@gmail.com>
2026-07-15 11:41:18 +0900

    filter 예제 - 오류

--- src/main/java/com/example/demo_0710/hero1/DemoApplication.java
+++ src/main/java/com/example/demo_0710/hero1/DemoApplication.java
@@ -14,6 +14,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 /*public final class DemoApplication {
@@ -51,25 +53,34 @@ public class DemoApplication {
         put(10, new Player(10, "Jaron", Side.DIRE, 1, 8, 7, new AgilityHero("Draw Rnger")));
         }
     };
-    public static void main(String[] args){
+    public static void main(String[] args) {
         List<Player> playerList = new ArrayList<>(PLAYERS.values());
 
         System.out.println(" -- Stream.forEach -- ");
         Stream<Player> playerStream = playerList.stream();
-        playerList.stream()
-        //        .forEach(player) -> {
-        //    System.out.println(player);
-        //};
-                .forEach(new Consumer<Player>()
-                        @Override
-                        public void accept(Player player){
-                            System void accept(Player.toString));
-                            System.out.println(player);
-        }
-                );
-        System.out.println(" -- for 향상된 -- ");
-        for (Player player : playerList){
-            System.out.println(player);
-        }
+
+        Stream<Player> intermediate = playerList.stream()
+                .filter(new Predicate<Player>() {
+                    @Override
+                    public boolean test(Player player) {
+                        System.out.println(" >> 첫 번째 중간 연산자가 실행됩니다 << ");
+                        return player.getSide().equals(Side.RADIANT);
+                    }
+                })
+                .filter(new Predicate<Player>() {
+                    @Override
+                    public boolean test(Player player) {
+                        System.out.println(" >> 두번째 중간 연산자가 실행됩니다 << ");
+                        return player.getKill() >= 5;
+                    }
+                });
+        System.out.println(" -- 중간 연산자들을 적용 완료 / 이제 최종 연산자를 통해 실행 -- ");
+        intermediate
+                .forEach(new Consumer<Player>() {
+                    @Override
+                    public void accept(Player player) {
+                        System.out.println(player);
+                    }
+                });
     }
 }

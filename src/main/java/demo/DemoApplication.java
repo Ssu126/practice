b767542d149c8332053ad4c84cae473abdd1346f
Suyoung <sueyoungkim69@gmail.com>
2026-07-20 14:45:28 +0900

    3-9 약식

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -3,7 +3,9 @@ package demo;
 import lombok.AccessLevel;
 import lombok.experimental.FieldDefaults;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
@@ -17,6 +19,7 @@ public class DemoApplication {
         heroMap.put(2, agility);
         //heroMap.put(3, intelligence);
 
+        System.out.println("-- Map<Integer, Hero> heroMap --");
         System.out.println("힘 속성 영웅");
         heroMap.get(1).attack();
         heroMap.get(1).ultimate();
@@ -24,5 +27,18 @@ public class DemoApplication {
         System.out.println("민첩 속성 영웅");
         heroMap.get(2).attack();
         heroMap.get(2).ultimate();
+
+        List<Hero> heroList = new ArrayList<>();
+        heroList.add(strength);
+        heroList.add(agility);
+
+        System.out.println("-- List<Hero> heroList --");
+        System.out.println("힘 속성 영웅");
+        heroList.get(0).attack();
+        heroList.get(0).ultimate();
+
+        System.out.println("민첩 속성 영웅");
+        heroList.get(1).attack();
+        heroList.get(1).ultimate();
     }
 }
